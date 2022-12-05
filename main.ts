2be12911657b88cc017983f8aca196fa422119e1
Yoshiya Hinosawa <stibium121@gmail.com>
2022-12-05 23:07:56 +0900

    update

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 import { serve } from "https://raw.githubusercontent.com/denoland/deno_std/main/http/server.ts";
-import "https://deno.land/std@0.154.0/node/module_all.ts";
+import "https://deno.land/std@0.168.0/node/module_all.ts";
 
 serve(() => new Response("ok2"));
