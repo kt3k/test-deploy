6294ab5999ade949db4229cb04e11651436cc340
Yoshiya Hinosawa <stibium121@gmail.com>
2022-12-06 01:35:09 +0900

    update

---
+++ deploy-check.ts
@@ -0,0 +1,19 @@
+import { retry } from "https://deno.land/std@0.167.0/async/retry.ts";
+
+const projectName = "kt3k-test-deploy";
+const branch = Deno.args[0]
+
+if (!branch) {
+  console.log("Usage: deno run deploy-check.ts <branch-name>");
+  Deno.exit(1);
+}
+
+const deployName = branch === "main" ? projectName : `${projectName}--${branch.replace(/\//g, "-")}`;
+
+retry(async () => {
+  const resp = await fetch(`${deployName}.deno.dev`);
+  await resp.text();
+  if (resp.status !== 200) {
+    throw new Error("failed");
+  }
+});
