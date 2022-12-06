b8d71e9b00225a90316154585069731fea6cf57d
Yoshiya Hinosawa <stibium121@gmail.com>
2022-12-06 13:58:41 +0900

    Feat/test pr deploy (#2)

---
+++ deploy-check.ts
@@ -0,0 +1,22 @@
+import { retry } from "https://deno.land/std@0.167.0/async/retry.ts";
+
+const projectName = "kt3k-test-deploy";
+const branch = Deno.args[0]
+
+if (!branch) {
+  console.log("Usage: deno run deploy-check.ts <branch-name>");
+  Deno.exit(1);
+}
+
+const deployName = branch === "main" ? projectName : `${projectName}--${branch.replace(/\//g, "-")}`;
+
+await retry(async () => {
+  console.log(`Checking https://${deployName}.deno.dev`)
+  const resp = await fetch(`https://${deployName}.deno.dev`);
+  await resp.text();
+  if (resp.status !== 200) {
+    console.log(`https://${deployName}.deno.dev is unavailable`);
+    throw new Error("failed");
+  }
+  console.log(`https://${deployName}.deno.dev is available`);
+});
